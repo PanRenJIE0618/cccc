55ec141961294c21e0ed0ac42ccdd4c9bf753d09
XiaoPan0618 <panrenjie0618@outlook.com>
2023-06-12 18:22:09 +0800

    Replace bottom_toolbar.vue

--- src/components/bottom_toolbar.vue
+++ src/components/bottom_toolbar.vue
@@ -1,32 +1,91 @@
 <template>
   <div class="fx-bottom_toolbar">
     <div class="fx-bottom_toolbar__functioning">
-      <div class="fx-bottom_toolbar__module" v-for="(item,index) in mod" :key="index">
-        <img :src=item.image alt="img">
-        <p>{{item.title}}</p>
+      <div class="fx-bottom_toolbar__module" v-for="(item,index) in mod" :key="index" @click="DPEmod(item)">
+        <img :src=activeImg(item) alt="img">
+        <p>{{ item.title }}</p>
       </div>
     </div>
   </div>
+  <!--  <HHM></HHM>-->
 </template>
 
 <script lang="ts" setup>
-import {reactive, ref} from "vue";
+import {reactive, ref, defineExpose, computed, defineEmits} from "vue";
+import HHM from "./HHM.vue";
+
+const emit = defineEmits(['clickChild']);
+const clickChild = () => {
+  let param = {
+    content: 'b'
+  };
+  //传递给父组件
+};
 import type {Ref} from "vue";
+import {useStore} from "../store";
+import Data_analysis from "./data_analysis/data_analysis.vue";
+import Emergency_drill from "./emergency/emergency_drill.vue";
+
+let active = {
+  Data: "fx-bottom_Data_analysis",
+  management: "fx-bottom_Human_housing_management",
+  drill: "fx-bottom_Emergency_drill"
+};
+
+let activeImg = computed<string>(() => (item: object) => {
+  let img;
+  if (selectMod.value === item.title) {
+    img = active[item.name] + "_active";
+  } else {
+    img = active[item.name];
+  }
+  return new URL("../assets/bottom/" + img + ".png", import.meta.url).href;
+});
+
+let selectMod = ref("");
 let mod = reactive<Array<object>>([
   {
-    title:"数据分析",
-    image: new URL("../assets/bottom/fx-bottom_Data_analysis.png", import.meta.url).href
+    title: "数据分析",
+    image: new URL("../assets/bottom/fx-bottom_Data_analysis.png", import.meta.url).href,
+    name: "Data"
   },
   {
-    title:"人房管理",
-    image: new URL("../assets/bottom/fx-bottom_Human_housing_management.png", import.meta.url).href
+    title: "人房管理",
+    image: new URL("../assets/bottom/fx-bottom_Human_housing_management.png", import.meta.url).href,
+    name: "management"
   },
   {
-    title:"应急演练",
-    image: new URL("../assets/bottom/fx-bottom_Emergency_drill.png", import.meta.url).href
+    title: "应急演练",
+    image: new URL("../assets/bottom/fx-bottom_Emergency_drill.png", import.meta.url).href,
+    name: "drill"
   }
-])
-console.log(mod)
+]);
+
+const store = useStore();
+
+let DPEmod: (mod: object) => string = function (mod: object): string {
+  if(selectMod.value === mod.title) {
+    selectMod.value = ""
+    emit('clickChild', selectMod.value);
+    store.setCurrentBottomSelected();
+    return
+  }
+  selectMod.value = mod.title;
+  switch (mod.title) {
+    case "数据分析":
+      store.setCurrentBottomSelected(Data_analysis);
+      break;
+    case "应急演练":
+      store.setCurrentBottomSelected(Emergency_drill);
+      break;
+    case "人房管理":
+      store.setCurrentBottomSelected(HHM);
+      break;
+  }
+  emit('clickChild', selectMod.value);
+};
+defineExpose({selectMod});
+console.log(mod);
 </script>
 
 <style scoped lang="less">
@@ -39,19 +98,23 @@ console.log(mod)
   bottom: 0;
   display: flex;
   justify-content: center;
+
   .fx-bottom_toolbar__functioning {
     width: 400px;
     position: fixed;
     bottom: 13px;
     display: flex;
     justify-content: center;
+
     .fx-bottom_toolbar__module {
       margin: 20px 20px 0 20px;
       position: relative;
+
       img {
         width: 82px;
         height: 68px;
       }
+
       p {
         font-size: 16px;
         text-align: center;
