16c1edab82f811c3521a881a2a5f10ba569f8f14
潘仁杰 <3428675395@qq.com>
2023-06-19 15:29:34 +0800

    Update iServer.ts

--- src/api/iServer.ts
+++ src/api/iServer.ts
@@ -4,14 +4,14 @@ import request from "../utils/request";
 // 获取三维数据
 export function get3Ddata() {
   return request({
-    url: 'http://221.130.54.58:8091/iserver/services/3D-FengxianV/rest/realspace/datas.rjson',
+    url: '/api/iserver/services/3D-FengxianV/rest/realspace/datas.rjson',
     method: 'get',
   });
 }
 // 获取三维数据
 export function get3Dblock() {
   return request({
-    url: 'http://221.130.54.58:8091/iserver/services/3D-local3DCache-HB84/rest/realspace/datas.rjson',
+    url: '/api/iserver/services/3D-local3DCache-HB84/rest/realspace/datas.rjson',
     method: 'get',
   });
 }
